4af6213e9483096da5626d8490af6afa52cb0044
Tomasz Mosur <tomasz@mosur.net>
2020-04-11 19:06:57 +0200

    insert bulk update

--- single-node-elasticsearch/scripts/insert_bulk.sh
+++ single-node-elasticsearch/scripts/insert_bulk.sh
@@ -1,4 +1,12 @@
-echo "INSERTING DATA"
 # echo "$(tail /app/documents_bulk_ready_to_add.json)"
+echo "CONFIGURING ELASTICSEARCH"
+echo "es.client.connection.request.timeout: 1000000" >> /usr/share/elasticsearch/config/elasticsearch.yml
+echo "es.client.connect.timeout: 1000000" >> /usr/share/elasticsearch/config/elasticsearch.yml
+echo "es.client.socket.timeout: 1000000" >> /usr/share/elasticsearch/config/elasticsearch.yml
+
+echo "INSERTING DATA"
 curl -XDELETE http://elasticsearch:9200/news_articles
-curl -XPUT http://elasticsearch:9200/news_articles/_bulk?pretty -H "Content-Type: application/x-ndjson"  --data-binary @"/app/documents_bulk_ready_to_add.json"
+curl -XPUT http://elasticsearch:9200/news_articles
+curl -XPOST http://elasticsearch:9200/news_articles/_bulk?pretty -H "Content-Type: application/x-ndjson"  --data-binary @"/app/documents_bulk_ready_to_add.json"
+
+echo "DATA INSERTED YEEEY"
