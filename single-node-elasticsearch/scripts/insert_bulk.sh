46b779a55c668ea2b3977e098aa14569cbf56515
Tomasz Mosur <tomasz@mosur.net>
2020-04-12 18:09:10 +0200

    mosurek final update

--- single-node-elasticsearch/scripts/insert_bulk.sh
+++ single-node-elasticsearch/scripts/insert_bulk.sh
@@ -1,12 +1,29 @@
 # echo "$(tail /app/documents_bulk_ready_to_add.json)"
-echo "CONFIGURING ELASTICSEARCH"
-echo "es.client.connection.request.timeout: 1000000" >> /usr/share/elasticsearch/config/elasticsearch.yml
-echo "es.client.connect.timeout: 1000000" >> /usr/share/elasticsearch/config/elasticsearch.yml
-echo "es.client.socket.timeout: 1000000" >> /usr/share/elasticsearch/config/elasticsearch.yml
+echo "sleeps for 99 sec"
+sleep 99
 
-echo "INSERTING DATA"
+echo "REWRITE NEWS_ARTICLE INDEX"
 curl -XDELETE http://elasticsearch:9200/news_articles
 curl -XPUT http://elasticsearch:9200/news_articles
-curl -XPOST http://elasticsearch:9200/news_articles/_bulk?pretty -H "Content-Type: application/x-ndjson"  --data-binary @"/app/documents_bulk_ready_to_add.json"
+
+echo "INSERTING DATA"
+echo "start	$(date +"%T")"
+
+for i in $(seq 1 21);
+do
+    echo "INSERTING DATA $i of 21"
+    echo "output/output$i.json"
+    echo $(curl -XGET http://elasticsearch:9200/news_articles/_stats/indexing) >> /app/indexing-times.txt
+    curl --write-out '%{http_code}' --silent --output -XPOST http://elasticsearch:9200/news_articles/_doc/_bulk?pretty -H "Content-Type: application/x-ndjson"  --data-binary @"/app/output/output$i.json" 
+    echo "$i	$(date +"%T")" >> /app/times.csv
+    echo "INSERTED DATA $i"
+done
+
 
 echo "DATA INSERTED YEEEY"
+sleep 10
+echo $(curl -XGET http://elasticsearch:9200/news_articles/_stats/indexing) >> /app/indexing-times.txt
+
+echo "RESULTS FILE:"
+cat /app/indexing-times.txt
+cat /app/times.csv
--- single-node-elasticsearch/scripts/transform_to_insert.py
+++ single-node-elasticsearch/scripts/transform_to_insert.py
@@ -4,13 +4,28 @@ from pathlib import Path
 
 input_file = sys.argv[1]
 output_file = sys.argv[2]
+LINES_IN_FILE = 15000
 
 print("TRANSFORMING FILE")
 with open(input_file, "r", encoding="utf8") as input_file:
-    with open(output_file, "w", encoding="utf8") as transformed:
-        print("files opened")
-        while True:
-            line = input_file.readline()
-            if not line: 
-                break
-            transformed.writelines(['{ "index" : { "_index" : "news_articles"} }\n', line])
+    fileNumber = 1
+    hasMoreLines = True
+    while(hasMoreLines):
+        filename="./output/" + output_file+str(fileNumber)+".json"
+        with open(filename, "w", encoding="utf8") as transformed:
+            j = 0 
+            print("{} opened".format(filename))
+            while j < LINES_IN_FILE:
+                line = input_file.readline()
+                if not line: 
+                    hasMoreLines=False
+                    break
+                # transformed.writelines(['{ "index" : { "_index" : "news_articles"} }\n', line])
+                transformed.writelines([line])
+                j=j+1
+            fileNumber = fileNumber + 1
+
+## HOW TO RUN SCRIPT
+# 1. CREATE FOLDER OUTPUT in this directory
+# 2. py transform_to_insert.py documents_bulk.json outputfielname
+# 3. output files structure is as follows: outputfilename + currentIndex + .json
