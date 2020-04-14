28f91ae54dabafc9dacd433c3789e8a14642d805
michallegut <michallegut@wp.pl>
2020-04-14 10:30:23 +0200

    Final final version v2.0.0

--- solr/6_test_queries.sh
+++ solr/6_test_queries.sh
@@ -1,12 +1,20 @@
 while true; do ./resources_usage.sh >> first_query_output.txt; usleep 1000; done&
+sleep 5
 curl "http://localhost:8983/solr/core/select?q=title%3Amarket&rows=1000" >> first_query_output.txt
+sleep 5
 cp first_query_output.txt results/first_query_output.txt
 while true; do ./resources_usage.sh >> second_query_output.txt; usleep 1000; done&
+sleep 5
 curl "http://localhost:8983/solr/core/select?q=title%3Amarket%20OR%20text%3Amarket%20OR%20url%3Amarket&rows=1000" >> second_query_output.txt
+sleep 5
 cp second_query_output.txt results/second_query_output.txt
 while true; do ./resources_usage.sh >> third_query_output.txt; usleep 1000; done&
+sleep 5
 curl "http://localhost:8983/solr/core/select?q=market&rows=1000" >> third_query_output.txt
+sleep 5
 cp third_query_output.txt results/third_query_output.txt
 while true; do ./resources_usage.sh >> fourth_query_output.txt; usleep 1000; done&
+sleep 5
 curl "http://localhost:8983/solr/core/select?q=Which%20is%20the%20largest%20market%20in%20the%20world?&rows=1000" >> fourth_query_output.txt
+sleep 5
 cp fourth_query_output.txt results/fourth_query_output.txt
--- solr/resources_usage.sh
+++ solr/resources_usage.sh
@@ -1,2 +1,1 @@
-grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print "CPU: " usage "%"}'
-free -m | awk 'NR==2{printf "Memory: %s/%sMB (%.3f%%)\n", $3,$2,$3*100/$2 }'
+docker stats --all --no-stream --format "table {{.CPUPerc}}\t{{.MemUsage}}\t{{.MemPerc}}" index_solr_1
