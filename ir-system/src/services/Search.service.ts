f9802f14b61c7381cf43743558ad985f66053b91
PrzemysławMorski <przemyslaw.morski@euvic.pl>
2020-05-26 21:26:09 +0200

    [ir-system] search service - added filters to load articles function

--- ir-system/src/services/Search.service.ts
+++ ir-system/src/services/Search.service.ts
@@ -2,31 +2,58 @@ import axios from "axios";
 import { Article } from "../models/Article";
 import { SearchResponse } from '../models/SearchResponse';
 import constants from '../config/constants';
-import { Filter } from "../pages/results-page/components/FiltersBar";
+import { Filter, Filters, FilterFields } from '../pages/results-page/components/FiltersBar';
 
 export default class SearchService {
 
-    async loadArticles(query: string, last?: Article): Promise<Article[]> {
-        let result: Article[] = [];
-        const body: any = {
-            size: 10,
-            sort: [
-                "_score",
-                { "uuid.keyword": "asc" }
-            ],
-            "query": {
-                "multi_match": {
-                    "query": query,
-                    "fields": [
-                        "title",
-                        "text",
-                        "entities.locations.name",
-                        "entities.organizations.name",
-                        "entities.persons.name"
-                    ]
+    private setFilters(body: any, filters?: Filters) {
+        if (filters) {
+            const elasticsearchFilters: any[] = [];
+            if (filters?.authors && filters.authors.length > 0) {
+                elasticsearchFilters.push({ "terms": { [FilterFields.Authors]: filters.authors.map(x => x.value) } });
+            }
+            if (filters?.organisations && filters.organisations.length > 0) {
+                elasticsearchFilters.push({ "terms": { [FilterFields.Organisations]: filters.organisations.map(x => x.value) } });
+            }
+            if (filters?.people && filters.people.length > 0) {
+                elasticsearchFilters.push({ "terms": { [FilterFields.People]: filters.people.map(x => x.value) } });
+            }
+            if (filters?.locations && filters.locations.length > 0) {
+                elasticsearchFilters.push({ "terms": { [FilterFields.Locations]: filters.locations.map(x => x.value) } });
+            }
+            if (filters?.dateFrom) {
+                elasticsearchFilters.push({ "terms": { [FilterFields.PublishDate]: filters.dateFrom } });
+            }
+            if (filters?.dateTo) {
+                elasticsearchFilters.push({ "range": { [FilterFields.PublishDate]: filters.dateTo } });
+            }
+
+            if (elasticsearchFilters.length > 0) {
+                body.bool = {
+                    "filter": elasticsearchFilters
                 }
             }
-        };
+        }
+    }
+
+    private setSortAndQuery(body: any, query: string, last?: Article) {
+        body["sort"] = [
+            "_score",
+            { "uuid.keyword": "asc" }
+        ];
+
+        body["query"] = {
+            "multi_match": {
+                "query": query,
+                "fields": [
+                    "title",
+                    "text",
+                    "entities.locations.name",
+                    "entities.organizations.name",
+                    "entities.persons.name"
+                ]
+            }
+        }
 
         if (last) {
             // if you add another field to sort then you need to add that field's value to search_after
@@ -34,6 +61,17 @@ export default class SearchService {
             // all field values for search_after can be accessed in the `last` object
             body.search_after = [last._score, last._source.uuid];
         }
+    }
+
+    async loadArticles(query: string, filters?: Filters, last?: Article): Promise<Article[]> {
+        let result: Article[] = [];
+
+        const body: any = {
+            size: 10
+        };
+
+        this.setFilters(body, filters);
+        this.setSortAndQuery(body, query);
 
         const response = await axios.post<SearchResponse<Article>>(constants.news_articles_search_url, body);
         console.log(response.data);
@@ -70,22 +108,11 @@ export default class SearchService {
         return result;
     }
 
-    async aggregateByQuery(query: string, field: string): Promise<Filter[]> {
+    async aggregateByQuery(query: string, field: string, filters?: Filters): Promise<Filter[]> {
         let result: Filter[] = [];
+
         const body: any = {
-            "size": 0,
-            "query": {
-                "multi_match": {
-                    "query": query,
-                    "fields": [
-                        "title",
-                        "text",
-                        "entities.locations.name",
-                        "entities.organizations.name",
-                        "entities.persons.name"
-                    ]
-                }
-            },
+            size: 0,
             "aggs": {
                 "genres": {
                     "terms": {
@@ -94,6 +121,9 @@ export default class SearchService {
                 }
             }
         };
+
+        this.setFilters(body, filters);
+        this.setSortAndQuery(body, query);
 
         const response = await axios.post<SearchResponse<Article>>(constants.news_articles_search_url, body);
         result = response.data.aggregations.genres.buckets
@@ -111,42 +141,5 @@ export default class SearchService {
         const response = await axios.post<{ count: number }>(constants.news_articles_count_url, query);
         console.log("got count response", response);
         return response.data.count;
-
     }
-
-    // async loadFilteredArticles(query: string, filters: Filters, last?: Article): Promise<Article[]> {
-    //     let result: Article[] = [];
-    //     const body: any = {
-    //         size: 10,
-    //         sort: [
-    //             "_score",
-    //             { "uuid.keyword": "asc" }
-    //         ],
-    //         "query": {
-    //             "multi_match": {
-    //                 "query": query,
-    //                 "fields": [
-    //                     "title",
-    //                     "text",
-    //                     "entities.locations.name",
-    //                     "entities.organizations.name",
-    //                     "entities.persons.name"
-    //                 ]
-    //             }
-    //         }
-    //     };
-
-    //     if (last) {
-    //         // if you add another field to sort then you need to add that field's value to search_after
-    //         // the order of fields in search_after needs to match the order of sort fields
-    //         // all field values for search_after can be accessed in the `last` object
-    //         body.search_after = [last._score, last._source.uuid];
-    //     }
-
-    //     const response = await axios.post<SearchResponse<Article>>(constants.news_articles_search_url, body);
-    //     console.log(response.data);
-    //     result = response.data.hits.hits;
-
-    //     return result;
-    // }
 }
