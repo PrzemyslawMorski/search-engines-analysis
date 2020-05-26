8580f08db2c0d9aacd1f63724e6bdd6818750871
PrzemysławMorski <przemyslaw.morski@euvic.pl>
2020-05-26 20:04:36 +0200

    [ir-system] aggregated filters displayed

--- ir-system/src/pages/results-page/components/FiltersBar.tsx
+++ ir-system/src/pages/results-page/components/FiltersBar.tsx
@@ -1,86 +1,90 @@
-import React, {useEffect, useState} from "react";
-import {Col, Container, Row} from "react-bootstrap";
-import Select, {ValueType} from 'react-select'
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Select, { ValueType } from 'react-select'
 import ReactDatePicker from "react-datepicker";
+import FiltersBarService, { FilterFields } from "./FiltersBar.service"
 
 type FiltersBarProps = Readonly<{
     onFiltersChange: (filters: Filters) => void
 }>
 
-const FiltersBar = ({onFiltersChange}: FiltersBarProps) => {
+const FiltersBar = ({ onFiltersChange }: FiltersBarProps) => {
 
-    const initialFilters: Filters = {locations: null, authors: null, organisations: null, dateFrom: null, dateTo: null}
+    const initialFilters: Filters = { locations: undefined, authors: undefined, people: undefined, organisations: undefined, dateFrom: undefined, dateTo: undefined }
+    const initialAvailableFilters: Filters = { locations: [], authors: [], organisations: [], people: [] };
 
     const [filters, setFilters] = useState<Filters>(initialFilters)
+    const [availableFilters, setAvailableFilters] = useState<Filters>(initialAvailableFilters)
 
-    const locations: Filter[] = [
-        {value: "test1", label: "to ma"},
-        {value: "test2", label: "byc"},
-        {value: "test3", label: "pobrane"},
-        {value: "test4", label: "z elastica"}
-    ]
-
-    const organisations: Filter[] = [
-        {value: "test1", label: "to ma"},
-        {value: "test2", label: "byc"},
-        {value: "test3", label: "pobrane"},
-        {value: "test4", label: "z elastica"}
-    ]
-
-    const authors: Filter[] = [
-        {value: "test1", label: "to ma"},
-        {value: "test2", label: "byc"},
-        {value: "test3", label: "pobrane"},
-        {value: "test4", label: "z elastica"}
-    ]
 
     const onLocationsChanged = (value: ValueType<Filter>) => {
-        setFilters(prevState => ({...prevState, locations: (value as Filter[])}))
+        setFilters(prevState => ({ ...prevState, locations: (value as Filter[]) }))
     }
 
     const onOrganisationsChanged = (value: ValueType<Filter>) => {
-        setFilters(prevState => ({...prevState, organisations: (value as Filter[])}))
+        setFilters(prevState => ({ ...prevState, organisations: (value as Filter[]) }))
+    }
+
+    const onPeopleChanged = (value: ValueType<Filter>) => {
+        setFilters(prevState => ({ ...prevState, people: (value as Filter[]) }))
     }
 
     const onAuthorsChanged = (value: ValueType<Filter>) => {
-        setFilters(prevState => ({...prevState, authors: (value as Filter[])}))
+        setFilters(prevState => ({ ...prevState, authors: (value as Filter[]) }));
     }
 
     const onDateFromChanged = (value: Date) => {
-        setFilters(prevState => ({...prevState, dateFrom: value}))
+        setFilters(prevState => ({ ...prevState, dateFrom: value }));
     }
 
     const onDateToChanged = (value: Date) => {
-        setFilters(prevState => ({...prevState, dateTo: value}))
+        setFilters(prevState => ({ ...prevState, dateTo: value }));
     }
 
     useEffect(() => {
         onFiltersChange(filters)
-    })
+    });
+
+    useEffect(() => {
+        const filtersBarService = new FiltersBarService();
+        filtersBarService.aggregateFilter(FilterFields.Locations, true).then((filters) =>
+            setAvailableFilters(prevState => ({ ...prevState, locations: filters })));
+        filtersBarService.aggregateFilter(FilterFields.Authors).then((filters) =>
+            setAvailableFilters(prevState => ({ ...prevState, authors: filters })));
+        filtersBarService.aggregateFilter(FilterFields.Organisations).then((filters) =>
+            setAvailableFilters(prevState => ({ ...prevState, organisations: filters })));
+        filtersBarService.aggregateFilter(FilterFields.People).then((filters) =>
+            setAvailableFilters(prevState => ({ ...prevState, people: filters })));
+    }, []);
 
     return (
         <Container>
             <Row>
                 <Col>
+                    <label htmlFor="authors">Authors</label>
+                    <Select id="authors" options={availableFilters.authors} isMulti onChange={onAuthorsChanged} />
+                </Col>
+                <Col>
                     <label htmlFor="locations">Locations</label>
-                    <Select id="locations" options={locations} isMulti onChange={onLocationsChanged}/>
+                    <Select id="locations" options={availableFilters.locations} isMulti onChange={onLocationsChanged} />
                 </Col>
                 <Col>
                     <label htmlFor="organisations">Organisations</label>
-                    <Select id="organisations" options={organisations} isMulti onChange={onOrganisationsChanged}/>
+                    <Select id="organisations" options={availableFilters.organisations} isMulti onChange={onOrganisationsChanged} />
                 </Col>
                 <Col>
-                    <label htmlFor="authors">Authors</label>
-                    <Select id="authors" options={authors} isMulti onChange={onAuthorsChanged}/>
-                </Col></Row>
+                    <label htmlFor="authors">People</label>
+                    <Select id="authors" options={availableFilters.people} isMulti onChange={onPeopleChanged} />
+                </Col>
+            </Row>
             <Row className={"mt-1"}>
                 <Col sm={2}>
                     <label htmlFor="dateFrom">From date</label>
-                    <ReactDatePicker id={"dateFrom"} className={"form-control"} value={filters.dateFrom?.toLocaleDateString()} onChange={onDateFromChanged}/>
+                    <ReactDatePicker id={"dateFrom"} className={"form-control"} value={filters.dateFrom?.toLocaleDateString()} onChange={onDateFromChanged} />
                 </Col>
                 <Col sm={2}>
                     <label htmlFor="dateTo">To date</label>
-                    <ReactDatePicker id={"dateTo"} className={"form-control"} value={filters.dateTo?.toLocaleDateString()} onChange={onDateToChanged}/>
+                    <ReactDatePicker id={"dateTo"} className={"form-control"} value={filters.dateTo?.toLocaleDateString()} onChange={onDateToChanged} />
                 </Col>
             </Row>
         </Container>
@@ -93,11 +97,12 @@ export type Filter = Readonly<{
 }>
 
 export type Filters = Readonly<{
-    locations: Filter[] | null,
-    organisations: Filter[] | null,
-    authors: Filter[] | null,
-    dateFrom: Date | null,
-    dateTo: Date | null
+    locations?: Filter[],
+    organisations?: Filter[],
+    people?: Filter[],
+    authors?: Filter[],
+    dateFrom?: Date,
+    dateTo?: Date
 }>
 
 export default FiltersBar
