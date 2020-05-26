bf504570fefe8baefa4598d7da49c5b23076cc25
Konrad Drozd <drozdkonrad@gmail.com>
2020-05-26 12:18:19 +0000

    Filters on the search base with mock data
    * Start implementing ResultsItem
    
    * Results page as a new route '/search'
    Refactor
    
    * Filters base

--- ir-system/src/index.tsx
+++ ir-system/src/index.tsx
@@ -5,11 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "react-datepicker/dist/react-datepicker.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
